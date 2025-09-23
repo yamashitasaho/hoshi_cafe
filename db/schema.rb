d2fc58f0c18c1459e52ba12e562e3cfb5218a149
山下紗穂 <yamashitasaho@yamashitasahonoMacBook-Air.local>
2025-09-23 12:02:51 +0900

    投稿一覧

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_18_133934) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_20_141453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "region", null: false
+    t.string "shop_name", null: false
+    t.text "body"
+    t.integer "rating", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_18_133934) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
