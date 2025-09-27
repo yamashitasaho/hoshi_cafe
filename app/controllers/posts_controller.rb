6bf38b19991e68895b7835f82d826c39f8249aa3
山下紗穂 <yamashitasaho@yamashitasahonoMacBook-Air.local>
2025-09-27 19:12:24 +0900

    フラッシュメッセージのUI改善と新規投稿通知を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,9 +11,9 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to posts_path, success: t("defaults.flash_message.created", item: Post.model_name.human)
+      redirect_to posts_path, notice: t("defaults.flash_message.created", item: Post.model_name.human)
     else
-      flash.now[:danger] = t("defaults.flash_message.not_created", item: Post.model_name.human)
+      flash.now[:alert] = t("defaults.flash_message.not_created", item: Post.model_name.human)
       render :new, status: :unprocessable_entity
       # エラーで422が返される
     end
