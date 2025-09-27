23169787bfab546d29669d17ea1a5ae5af49bfb6
山下紗穂 <yamashitasaho@yamashitasahonoMacBook-Air.local>
2025-09-27 14:14:45 +0900

    新規投稿の保存、一覧機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,21 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: t("defaults.flash_message.created", item: Post.model_name.human)
+    else
+      flash.now[:danger] = t("defaults.flash_message.not_created", item: Post.model_name.human)
+      render :new, status: :unprocessable_entity
+      # エラーで422が返される
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:region, :shop_name, :rating, :body)
+  end
 end
