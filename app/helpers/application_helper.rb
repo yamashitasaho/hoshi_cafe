e169887f7f685a5c11a222cb7bd689acb2542f6b
山下紗穂 <yamashitasaho@yamashitasahonoMacBook-Air.local>
2026-01-15 19:21:11 +0900

    TwitterカードでOGP画像を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
-end
+    def get_twitter_card_info(post)
+      twitter_card = {}
+      
+      twitter_card[:card] = 'summary_large_image'
+      twitter_card[:url] = post_url(post)
+      twitter_card[:title] = "#{post.shop_name} - hoshi cafe"
+      twitter_card[:description] = "#{post.region}のカフェ | ★#{post.rating}"
+      
+      # 画像の有無で分岐
+      if post.image.attached?
+        twitter_card[:image] = url_for(post.image)
+      else
+        twitter_card[:image] = image_url('coffee.jpg')
+      end
+      
+      twitter_card
+    end
+  end
